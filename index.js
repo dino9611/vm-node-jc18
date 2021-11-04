00bad6d003fe55953c1745e9e72adc961ae18f19
dino9611 <aldinorahman36@gmail.com>
2021-11-04 10:49:47 +0700

    init vm

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(
+    "<h1>welcome vm nodejs</h1>"
+  );
+});
+
+// console.log("dari docker " + process.env.NAME);
+app.listen(5000, () => console.log("app jalan di 5000 "));
