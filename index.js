388649e60fce7ddfa9d65183342a3f110bec05ed
dino9611 <aldinorahman36@gmail.com>
2021-11-05 15:10:52 +0700

    SCRUM-6 update console.log

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require("express");
 
 const app = express();
 
+
 app.get("/", (req, res) => {
   res.send(
-    "<h1>welcome vm nodejs</h1>"
+    "<h1>welcome vm nodejs </h1>"
   );
 });
 
 // console.log("dari docker " + process.env.NAME);
-app.listen(5000, () => console.log("app jalan di 5000 "));
+app.listen(5000, () => console.log("app jalan di 5000  🚀🚀"));
